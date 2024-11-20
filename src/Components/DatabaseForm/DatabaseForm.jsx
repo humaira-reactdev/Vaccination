2c7869954c5780ee9adba51d03bfbc8b2d6ed053
Humaira Islam <ihumaira.webfront@gmail.com>
2024-11-20 22:08:53 +0600

    Result check for covid 19 section added

--- src/Components/DatabaseForm/DatabaseForm.jsx
+++ src/Components/DatabaseForm/DatabaseForm.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import './DatabaseForm.css'
 
 const DatabaseForm = () => {
   return (
-    <div>DatabaseForm</div>
-  )
-}
+    <>
+    <div className='databaseform'>
+      <div className='container'>
+          <div className='heading'>
+              <h2>Check your COVID-19 result on our Database</h2>
+          </div>
+          {/* Form Section */}
+          <div className='form'>
+            <div className="inputFields">
+              <input
+                type='text'
+                placeholder='Name'
+                className='input-field'
+              />
+              <input
+                type='text'
+                placeholder='NIK Number'
+                className='input-field'
+              />
+              <button className='btn-check'>
+                Check
+              </button>
+            </div> 
+            {/* Footer Section */}
+           <div className='footer'>
+            <p>
+              Need a certificate for your COVID-19 result? Please click{' '}
+              <a href='#' className='footer-link'>
+                 here
+              </a>
+            </p>
+          </div>                 
+          </div>                     
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default DatabaseForm
+export default DatabaseForm;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Banner from '../Components/Banner/Banner'
 import ResponsiveBanner from '../Components/ResponsiveBanner/ResponsiveBanner'
+import DatabaseForm from '../Components/DatabaseForm/DatabaseForm'
 
 const Home = () => {
   return (
     <>
       <Banner/>
       <ResponsiveBanner/>
-      
+      <DatabaseForm/>
     </>
   )
 }
