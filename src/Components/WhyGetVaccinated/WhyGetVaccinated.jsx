fd27f7536d096eea145d9e6050afc1fe78c79e17
Humaira Islam <ihumaira.webfront@gmail.com>
2024-11-22 23:13:33 +0600

    WHy get vaccinated section started

---
+++ src/Components/WhyGetVaccinated/WhyGetVaccinated.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './WhyGetVaccinated.css'
+
+const WhyGetVaccinated = () => {
+  return (
+    <>
+    <div className='why'>
+        <div className="container">
+            <div className="text">
+                <h2 className='vaccinatedHeading'>Why get vaccinated today?</h2>
+                <p className='textVaccinated'>Magna adipiscing at elit at ornare lectus nibh lorem. 
+                Ac, sed ac lorem pellentesque vestibulum risus matti. 
+                In molestie condimentum malesuada non.</p>
+            </div>       
+            <div className='cards'>
+                <div className='immune'>
+                    
+
+                </div>
+                <div className='minimize'>
+
+                </div>
+                <div className='protect'>
+
+                </div>
+            </div>     
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default WhyGetVaccinated
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../Components/Banner/Banner'
 import ResponsiveBanner from '../Components/ResponsiveBanner/ResponsiveBanner'
 import DatabaseForm from '../Components/DatabaseForm/DatabaseForm'
+import WhyGetVaccinated from '../Components/WhyGetVaccinated/WhyGetVaccinated'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner/>
       <ResponsiveBanner/>
       <DatabaseForm/>
+      <WhyGetVaccinated/>
     </>
   )
 }
