3fcb7c837dc6a335cd512400fbb5e75cd12a9c03
Humaira Islam <ihumaira.webfront@gmail.com>
2024-11-24 20:33:40 +0600

    WhyGetVaccinated completed

--- src/Components/WhyGetVaccinated/WhyGetVaccinated.jsx
+++ src/Components/WhyGetVaccinated/WhyGetVaccinated.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './WhyGetVaccinated.css'
+import { LuShield } from "react-icons/lu";
+import { FaArrowRight } from "react-icons/fa6";
+import { GoPeople } from "react-icons/go";
+import { FiThermometer } from "react-icons/fi";
 
 const WhyGetVaccinated = () => {
   return (
@@ -14,13 +18,40 @@ const WhyGetVaccinated = () => {
             </div>       
             <div className='cards'>
                 <div className='immune'>
-                    
-
+                  <div className='shieldbg'>
+                    <LuShield className='shieldIcon'/>                    
+                  </div>
+                  <h3 className='immuneHeading'>Protects your immune 
+                    system against viruses</h3>
+                  <p className='immuneText'>Velit ut consectetur mauris, orci amet, faucibus. 
+                    Sit turpis fringilla ipsum pretium,
+                    dictum. Dolor eget vel nulla lorem ac.
+                    </p>
+                    <div className='readMore'>
+                      <h3>Read More</h3>
+                      <FaArrowRight />
+                    </div>                       
                 </div>
                 <div className='minimize'>
-
+                  <div className='peoplebg'>
+                    <GoPeople className='peopleicon'/>
+                  </div>
+                  <h3 className='minimizeheading'>Minimize the spread
+                  of the Virus</h3> 
+                  <div className='readMore'>
+                    <h3>Read More</h3>
+                    <FaArrowRight />
+                  </div>          
                 </div>
                 <div className='protect'>
+                <div className='protectbg'>
+                    <FiThermometer className='protecticon'/>
+                  </div>
+                  <h3 className='protectheading'>Protect yourself</h3> 
+                  <div className='readMore'>
+                    <h3>Read More</h3>
+                    <FaArrowRight />
+                </div>
 
                 </div>
             </div>     
